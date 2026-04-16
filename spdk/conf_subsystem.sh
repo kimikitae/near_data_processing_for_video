94ae911d67faa0b4d874205ccdf0e880860b56a6
kimikitae <kcm8619@gmail.com>
2026-04-16 11:56:26 +0900

    Remove comment

--- spdk/conf_subsystem.sh
+++ spdk/conf_subsystem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo sudo scripts/rpc.py nvmf_create_transport -t TCP -u 131072 -m 20 -c 8192 -i 2097152 # SPDK의 TCP Transport 스펙을 확인하여 구성하세요. 좌측에 나와있는 것은 참고만 하세요.
+sudo sudo scripts/rpc.py nvmf_create_transport -t TCP -u 131072 -m 20 -c 8192 -i 2097152
 sudo scripts/rpc.py bdev_nvme_attach_controller -b nvme1 -t PCIe -a 0000:82:00.0
 sudo scripts/rpc.py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode1 -a -s SPDK00000000000001 -d nvme1n1
 sudo scripts/rpc.py nvmf_subsystem_add_ns nqn.2016-06.io.spdk:cnode1 nvme1n1
